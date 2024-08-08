57bd271e926d3b5e086431da9ddd4de702bff762
v012345 <v138468@gmail.com>
2024-08-08 14:56:42 +0800

    init

---
+++ NightOwlToolV2CS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NightOwlToolV2CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
